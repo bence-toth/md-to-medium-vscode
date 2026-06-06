4f0e8dcf2ad563671652f654b9b78a1991606610
Bence A. Toth <tothab@gmail.com>
2026-06-06 20:38:03 +0200

    feat: wire status bar item into extension activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import { copyAsMediumHtml } from './commands.js';
 import { disposeOutputChannel } from './outputChannel.js';
+import { createStatusBarItem } from './statusBar.js';
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand('mdToMedium.copyAsMediumHtml', copyAsMediumHtml),
   );
+  createStatusBarItem(context);
 }
 
 export function deactivate(): void {
