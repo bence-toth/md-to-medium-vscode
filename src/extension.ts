6f5f12574c4a6f00a087fa89c211d3bc7a5aa604
Bence A. Toth <tothab@gmail.com>
2026-06-06 20:34:37 +0200

    chore: add empty src/extension.ts with activate/deactivate stubs

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export function activate(_context: vscode.ExtensionContext): void {
+  // wired in subsequent phases
+}
+
+export function deactivate(): void {
+  // intentionally empty
+}
